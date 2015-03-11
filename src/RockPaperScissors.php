4c1930b370d6dd1952ee80819b8c71e9f10826a6
Daniel Toader & Geoff Winner <student@epicodus.com>
2015-03-11 11:58:07 -0700

    initialize directory structure

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -7,7 +7,7 @@
                 return "Player 1";
             } else if ($player2 == "rock" && $player1 == "scissors" || $player1 == "rock" && $player2 == "paper" || $player1 == "paper" && $player2 == "scissors") {
                 return "Player 2";
-            } else if ($player1 == "rock" && $player2 == "rock" || $player1 == "paper" && $player2 == "paper" || $player1 == "scissors" && $player2 == "scissors") {
+            } else {
                 return "Nobody";
             }
         }
