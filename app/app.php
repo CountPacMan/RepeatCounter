16ced83e9434247a9fa2b49c502dd3e99fc09354
Daniel Toader & Geoff Winner <student@epicodus.com>
2015-03-11 17:05:06 -0700

    add front end

--- app/app.php
+++ app/app.php
@@ -1,6 +1,6 @@
 <?php
   require_once __DIR__."/../vendor/autoload.php";
-  require_once __DIR__."/../src/RockPaperScissors.php";
+  require_once __DIR__."/../src/FindReplace.php";
 
   $app = new Silex\Application();
   $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
@@ -10,23 +10,12 @@
   });
 
   $app->post("/generate", function() use ($app) {
-    $player1 = $_POST['player1'];
-    $player2 = $_POST['player2'];
-    $newGame = new FindReplace();
-    $result = $newGame->play($player1, $player2);
-    return $app['twig']->render('generate.twig', array('player1' => $player1, 'player2' => $player2, 'result' => $result, 'computer' => false));
-  });
-
-  $app->get("/computer", function() use ($app) {
-    return $app['twig']->render('computer.twig');
-  });
-
-  $app->post("/computerGenerate", function() use ($app) {
-    $player1 = $_POST['player1'];
-    $newGame = new FindReplace();
-    $player2 = $newGame->playComputer($player1);
-    $result = $newGame->play($player1, $player2);
-    return $app['twig']->render('generate.twig', array('player1' => $player1, 'player2' => $player2, 'result' => $result, 'computer' => true));
+    $big_string = $_POST['big_string'];
+    $to_replace = $_POST['to_replace'];
+    $replacement = $_POST['replacement'];
+    $new_findReplace = new FindReplace();
+    $new_sentence = $new_findReplace->replace($big_string, $to_replace, $replacement);
+    return $app['twig']->render('generate.twig', array('big_string' => $big_string, 'to_replace' => $to_replace, 'replacement' => $replacement, 'new_sentence' => $new_sentence));
   });
 
   return $app;
