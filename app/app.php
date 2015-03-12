90dccb7ec8f275b9e0ee0cc543af7893e7760b0d
Daniel Toader & Patrick Fiorentino <student@epicodus.com>
2015-03-12 10:49:09 -0700

    add views

--- app/app.php
+++ app/app.php
@@ -1,6 +1,6 @@
 <?php
   require_once __DIR__."/../vendor/autoload.php";
-  require_once __DIR__."/../src/FindReplace.php";
+  require_once __DIR__."/../src/ScrabbleScore.php";
 
   $app = new Silex\Application();
   $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
@@ -10,12 +10,9 @@
   });
 
   $app->post("/generate", function() use ($app) {
-    $big_string = $_POST['big_string'];
-    $to_replace = $_POST['to_replace'];
-    $replacement = $_POST['replacement'];
-    $new_findReplace = new FindReplace();
-    $new_sentence = $new_findReplace->replace($big_string, $to_replace, $replacement);
-    return $app['twig']->render('generate.twig', array('big_string' => $big_string, 'to_replace' => $to_replace, 'replacement' => $replacement, 'new_sentence' => $new_sentence));
+    $new_scrabble = new ScrabbleScore;
+    $score = $new_scrabble->getScore($_POST['letters']);
+    return $app['twig']->render('generate.twig', array('letters' => $_POST['letters'], 'score' => $score));
   });
 
   return $app;
