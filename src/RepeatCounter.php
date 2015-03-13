3fc621b83fc0ab9fa5a03831e40529bcc0f152fd
Daniel Toader <danis@DaniAir.local>
2015-03-13 09:42:26 -0700

    add class functionality to pass count matches. Test 1 pass.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -3,8 +3,11 @@
     class RepeatCounter {
 
       function getCount($haystack, $needle) {
-        
-
+        $count = 0;
+        foreach (str_split($haystack) as $word) {
+          $word == $needle ? $count++ : "";
+        }
+        return $count;
       }
     }
  ?>
